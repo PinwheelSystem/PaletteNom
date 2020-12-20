0ffca5f06919f10e04f8747fccc6ee33ff728427
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-12-19 22:18:31 -0400

    define palettes in images

--- palettenom.go
+++ palettenom.go
@@ -19,9 +19,20 @@ func New() PaletteNom {
 	return PaletteNom{make([]color.Color, 64)}
 }
 
-func (p *PaletteNom) Load(filename string) []color.Color {
-	file, _ := os.Open(filename)
-	image, _, _ := image.Decode(file)
+func (p *PaletteNom) Load(filename string) ([]color.Color, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	image, _, err := image.Decode(file)
+	if err != nil {
+		return err
+	}
+
+	if image.Width > 64 || image.Height > 64 {
+		errors.New("palette image must be 8x8")
+	}
 
 	for y := 0; y < 8; y++ {
 		for x := 0; x < 8; x++ {
